9a76eeeabcbde93e0b4d031572208bec760d1819
Abdulrhman Mustafa <abdulrhmanmustafa00@gmail.com>
2025-05-30 09:14:12 +0300

    edit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -66,7 +66,7 @@ const handleSubmit = async (e) => {
             </div>
             <div className="lg:flex max-lg:flex-col lg:justify-between bg-zinc-800 rounded-xl [box-shadow:0_-20px_80px_-40px_#ffffff3f_inset,0_0_20px_-5px_rgba(255,255,255,0.1)]">
                 <form onSubmit={handleSubmit} className=' max-lg:w-full lg:w-2/3 mb-14 py-10 px-5 rounded-xl flex flex-col gap-4'>
-                    <h1 className="getInTouch text-3xl font-bold mb-3">Send Me a Message</h1>
+                    <h1 className="getInTouch text-xl font-medium mb-3">Send Me a Message</h1>
                     <div className="name flex flex-col">
                         <label className="mb-3" htmlFor="name">Name</label>
                         <input className='bg-zinc-900 border-0 outline-0 px-4 py-3 rounded-xl' type="text" name="name" id="name" required placeholder='Your Name' />
@@ -82,22 +82,22 @@ const handleSubmit = async (e) => {
                     <button type='submit' className='flex items-center justify-center w-38 mx-auto rounded-xl bg-zinc-900 px-4 py-3 border-2 border-transparent  gap-2 hover:bg-transparent hover:border-2 hover:border-zinc-400 transition-all cursor-pointer'>Submit <div className="arrow transition duration-300"><HiOutlineMail /></div></button>
                 </form>
                 <div className="contact-info  bg-zinc-950 max-lg:w-full lg:w-1/2 flex flex-col gap-4 px-5 py-10">
-                    <h1 className="getInTouch text-3xl font-bold mb-3">Contact Information</h1>
+                    <h1 className="getInTouch text-xl font-medium mb-3">Contact Information</h1>
                     <p className="text-zinc-400">If you have any questions or inquiries, please don't hesitate to contact me.</p>
                     <div className="image">
                         <img src={formImage} className=" object-cover" alt="form" loading='lazy'/>
                     </div>
                     <div className="gap-10 flex flex-col items-start pt-10">
                         <div className="flex items-center justify-center gap-2">
-                            <MdLocationPin className='text-zinc-400 text-2xl font-bold' />
+                            <MdLocationPin className='text-zinc-400 text-xl font-bold' />
                             <p className="text-zinc-400">Alex, Egypt</p>
                         </div>
                         <div className="flex items-center justify-center gap-2">
-                            <MdOutlineWhatsapp className='text-zinc-400 text-2xl font-bold' />
+                            <MdOutlineWhatsapp className='text-zinc-400 text-xl font-bold' />
                             <p className="text-zinc-400">+201002603554</p>
                         </div>
                         <div className="flex items-center justify-center gap-2">
-                            <HiOutlineMail className='text-zinc-400 text-2xl font-bold' />
+                            <HiOutlineMail className='text-zinc-400 text-xl font-bold' />
                             <p className="text-zinc-400">abdurahmanmustafa2910@gmail.com</p>
                         </div>
                     </div>
